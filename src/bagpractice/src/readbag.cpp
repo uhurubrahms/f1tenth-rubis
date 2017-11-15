77420024066e5e1ec2f386786cdf47594e2e6dae
uhurubrahms <brahms1213@gmail.com>
2017-11-15 19:08:05 +0900

    figured out how to process ranges array

--- src/bagpractice/src/readbag.cpp
+++ src/bagpractice/src/readbag.cpp
@@ -21,41 +21,45 @@ try{
 }catch(int e){
   cout << "Exception occurred when opening a bag file - errno: " << e << endl;
 }
-//okay, so no error while opening a file.
 
 vector<string> topics;
-//topics.push_back(string("chatter"));//??
-//topics.push_back(string("numbers"));//??
 string laser_data = "/scan"; 
 // ==> something was wrong with this topic name
 // so it was not '/LaserScan' or just 'scan'
 topics.push_back(laser_data);
 
 rosbag::View view(bag, rosbag::TopicQuery(topics));
-
-//cout << "before foreach .... " << endl;
-
-//so message instance is void.. not looping.
+int count = 0; 
 foreach(rosbag::MessageInstance const m, view){
-  //std_msgs::String::ConstPtr s = m.instantiate<std_msgs::String>();
-  cout << m.getTopic() << endl;   
+  // cout << m.getTopic() << endl;   
   sensor_msgs::LaserScan::ConstPtr s = m.instantiate<sensor_msgs::LaserScan>();
+  if(s != NULL){
+    if(count == 0){
+      cout << "range_min: " << s->range_min << endl;
+      cout << "range_max: " << s->range_max << endl;
+    }
   
-  cout << "in foreach...." << endl;
+    // Prints header
+    cout << count+1 << ") " << endl << s->header;
   
-  if(s!=NULL){
-    //cout << s->data << endl;
-    cout << "range_min: " << s-> range_min << endl;
-  }
 
-/*
-  std_msgs::Int32::ConstPtr i = m.instantiate<std_msgs::Int32>();
-  if(i!=NULL){
-    cout << i->data << endl;
+    // Prints ranges array data from 0 to 1080
+    // ==> 'ranges' data type: vector, size(): 1081
+    // but 65.53299713134766 becomes 65.533
+
+    int i;
+    int len_data = (s->ranges).size();
+    for(i = 0; i < len_data; i++){
+      cout << i+1 << " : " << (s->ranges).at(i) << endl;
+    }
+
+    count ++;
   }
-*/
-}
-cout << "what??????" << endl;
+} // end foreach
+cout << "-----Done scanning the bag file.-----" << endl;
+cout << "# of data: " << count << endl;
+// ==> 2637 messages.
+
 bag.close();
 return (0);
 }
